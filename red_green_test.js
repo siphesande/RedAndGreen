8a94cb698e828dd8b215bcde1ef1805e211b0239
siphesande <siphesande@projectcodex.co>
2015-06-17 16:09:37 +0200

    Delete red_green_test.js

--- red_green_test.js
+++
@@ -1,9 +0,0 @@
-var rg = new RedOrGreen("myElement");
-
-  rg.makeGreen();
-    /*myElements background color should be set to red green now*/
-      window.alert("myElement background-color = green");
-
-  rg.makeRed();
-   /*myElements background should be set to red now*/
-      window.alert("myElement background-color = red");
